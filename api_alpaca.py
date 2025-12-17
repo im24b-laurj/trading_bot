94753e6bc1911268812fb808de9f15d91b926bf3
Jonas_Laur <laurj@bzz.ch>
2025-12-17 10:54:36 +0100

    Initial commit

--- api_alpaca.py
+++ api_alpaca.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+import yfinance as yf
+import pandas as pd
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+symbol = "AAPL"
+df = yf.download(symbol, period="7d", interval="5m")
+print(df.tail())
 
 
-# Press the green button in the gutter to run the script.
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    x = 2
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
