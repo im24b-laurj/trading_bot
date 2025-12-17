29b0bfdda63220ce10c2eb41692db8d91cc9427e
Jonas_Laur <laurj@bzz.ch>
2025-12-17 11:06:23 +0100

    Initial commit

--- api_alpaca.py
+++ api_alpaca.py
@@ -2,8 +2,21 @@ import yfinance as yf
 import pandas as pd
 
 symbol = "AAPL"
-df = yf.download(symbol, period="7d", interval="5m")
+df = yf.download(symbol, period="1d", interval="5m")
+print(df)
 print(df.tail())
+previous_high = 0
+previous_low = 0
+current_high = 0
+current_low = 0
+
+for i in df.intertuples:
+    if i.high > previous_high:
+        previous_high = i
+    if i.Close < previous_low:
+        previous_low = i
+
+print(previous_high, previous_low)
 
 
 
